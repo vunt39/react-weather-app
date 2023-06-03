c838099bfdf44f223997211a4406dcf2ce3dc27c
vu nguyen <vunt39@gmail.com>
2023-06-03 14:23:36 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,107 +1,41 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
-import Weather from './components/weather';
-import Forecast from './components/forecast';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import Search from './components/search/search'
+import CurrentWeather from './components/curren-weather/current-weather';
+import Forecast from './components/forecast/forecast';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
+import { useState } from 'react';
 
-export default function App(){
-  
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
-  const [weatherData, setWeatherData] = useState([]);
-  const [forecast, setForecast] = useState([]);
-  const [error, setError] = useState(null);  
-  
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
 
-    getWeather(lat, long)
-    .then(weather => {
-      setWeatherData(weather);
-      setError(null);
-    })
-    .catch(err => {
-      setError(err.message);
-    });
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ")
 
-    getForecast(lat, long)
-      .then(data => {
-        setForecast(data);
-        console.log('22222')
-        setError(null);
+    console.log(searchData.value)
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({city: searchData.label, ...weatherResponse})
+        setForecast({ city: searchData.label, ...forecastResponse})
       })
-      .catch(err => {
-        console.log('3333')
-        setError(err.message);
-      });
-  }, [lat, long, error]);
-
-  function handleResponse(response) {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Please Enable your Location in your browser!");
-    }
+      .catch((err) => console.log(err));
   }
 
-  function getWeather(lat, long) {
-    console.log('yyyyyy')
-    return fetch(
-      `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-    )
-      .then(res => handleResponse(res))
-      .then(weather => {
-        if (Object.entries(weather).length) {
-          const mappedData = mapDataToWeatherInterface(weather);
-          return mappedData;
-        }
-      });
-  }
-  
-  function getForecast(lat, long) {
-    return fetch(
-      `${process.env.REACT_APP_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-    )
-      .then(res => handleResponse(res))
-      .then(forecastData => {
-        if (Object.entries(forecastData).length) {
-          return forecastData.list
-            .filter(forecast => forecast.dt_txt.match(/09:00:00/))
-            .map(mapDataToWeatherInterface);
-        }
-      });
-  }
+  console.log(forecast)
 
-  function mapDataToWeatherInterface(data) {
-    const mapped = {
-      date: data.dt * 1000, // convert from seconds to milliseconds
-      description: data.weather[0].main,
-      temperature: Math.round(data.main.temp),
-    };
-  
-    // Add extra properties for the five day forecast: dt_txt, icon, min, max
-    if (data.dt_txt) {
-      mapped.dt_txt = data.dt_txt;
-    }
-    return mapped;
-  }
   return (
-    <div className='App'>
-      {(typeof weatherData.main != 'undefined')  ? (
-        <div>
-          <Weather weatherData={weatherData}/>
-          <Forecast forecast={forecast} weatherData={weatherData}/>
-        </div>
-      ): (
-        <div>
-          <Dimmer active>
-            <Loader>Loading..</Loader>
-          </Dimmer>
-        </div>
-      )}
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+      {currentWeather &&  <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,13 @@
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cc33702cc1msh86f07e522ac2ca1p1b15cbjsn46c6a0e63470',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '5e968201c551d7ffb0f426991b36e5cf'
